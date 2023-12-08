fe14ba5b4c16c181b4dc8e44721be528f5a1b9ea
Raul <raul.alex.dragu@gmail.com>
2023-12-08 06:47:50 +0400

    A basic component.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 use leptos::*;
 
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <button on:click = move |_| set_count.update(|n| *n += 1)>
+            "Click me"
+        </button>
+        <p>
+            <strong>"Value: "</strong>
+            { move || count.get() }
+        </p>
+    }
+}
+
 fn main() {
-    mount_to_body(|| view! {<p>"Hello, world!"</p>})
+    mount_to_body(|| view! { <App/> })
 }
