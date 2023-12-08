98e4feaa69790e21f8286d6aec5be7b2528fd72c
Raul <raul.alex.dragu@gmail.com>
2023-12-08 07:30:51 +0400

    Dynamic attributes.

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,27 @@ use leptos::*;
 #[component]
 fn App() -> impl IntoView {
     let (count, set_count) = create_signal(0);
+    let double_count = move || count() * 2;
 
     view! {
-        <button on:click = move |_| set_count.update(|n| *n += 1)>
+        <button
+            on:click = move |_| set_count.update(|n| *n += 1)
+            class:red = move || count() % 2 == 1
+        >
             "Click me"
         </button>
-        <p>
-            <strong>"Value: "</strong>
-            { move || count.get() }
-        </p>
+        <br/>
+        <progress
+            max="50"
+            value=count
+        />
+        <br/>
+        <progress
+            max="50"
+            value=double_count
+        />
+        <p>"Count: " {count}</p>
+        <p>"Double count: " {double_count}</p>
     }
 }
 
