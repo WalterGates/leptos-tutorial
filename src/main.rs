628d70fd2ec9a8dc68125195d19ad81e3b43b130
Raul <raul.alex.dragu@gmail.com>
2023-12-08 22:55:42 +0400

    Components and props.

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,37 @@ fn App() -> impl IntoView {
 
     view! {
         <button
-            on:click = move |_| set_count.update(|n| *n += 1)
-            class:red = move || count() % 2 == 1
+            on:click=move |_| set_count.update(|n| *n += 1)
+            class:red=move || count() % 2 == 1
         >
             "Click me"
         </button>
         <br/>
-        <progress
-            max="50"
-            value=count
-        />
+        <ProgressBar progress=count/>
         <br/>
-        <progress
-            max="50"
-            value=double_count
-        />
+        <ProgressBar max=100 progress=double_count/>
         <p>"Count: " {count}</p>
         <p>"Double count: " {double_count}</p>
+    }
+}
+
+/// Shows progress toward a goal.
+#[component]
+fn ProgressBar<F>(
+    /// The maximum value of the progress bar.
+    #[prop(default = 50)]
+    max: u16,
+    /// How much progress should be displayed.
+    progress: F,
+) -> impl IntoView
+where
+    F: Fn() -> i32 + 'static,
+{
+    view! {
+        <progress
+            max=max
+            value=progress
+        />
     }
 }
 
